e438904d35d5fdd612c8d9048f3a737d8aba04a5
Francesco <francesco.ferruccio88@gmail.com>
2020-04-09 13:02:21 +0200

    refactoring codice precedente per implementare nuove feature

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 // BoolzApp
 // DESCRIZIONE:
+//
 // Milestone 1:
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse (quindi tutto statico);
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde (quindi solo quello NON aggiungiamo dinamicamente anche quello bianco di risposta)
-
+//
 // Milestone 2:
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+//
+// Milestone 3:
+// Click sul contatto mostra la conversazione del contatto cliccato,
+// è possibile inserire nuovi messaggi per ogni conversazione
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
 
 $(document).ready (function() {
   // DICHIARAZIONE VARIABILI GLOBALI -------------------------------------------
@@ -53,7 +59,7 @@ $(document).ready (function() {
       } else {
         $(this).show();
       }
-    })
+    });
   }
 
   // INIZIO CODICE -------------------------------------------------------------
@@ -62,23 +68,23 @@ $(document).ready (function() {
   btnInvioMsg.click(inviaMsg);
 
   // invio il msg premendo "INVIO" sulla tastiera
-  $(document).keydown(function (event) {
-    if(event.which == 13) {
-      inviaMsg();
+  $(input).keydown(
+    function (event) {
+      if(event.which == 13) {
+        inviaMsg();
+      }
     }
-  });
+  );
 
   // cambio l'icona del microfono quando sto scrivendo un messaggio
-  input.on({
-    focusin: function() {
-      swapIconSend();
-    }, focusout: function() {
-      swapIconSend();
+  input.on("focusin focusout",
+  function () {
+    swapIconSend();
     }
-  });
+  );
 
   // eseguo la ricerca ogni volta che l'utente inizia a scrivere qualcosa sull'input search
-  $(document).keyup(ricerca);
+  $(searchInput).keyup(ricerca);
 
 
 
